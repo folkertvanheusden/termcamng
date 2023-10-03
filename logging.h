1409349d86022f013de779f53c43c463f38c18a9
folkert van heusden <mail@vanheusden.com>
2023-10-03 12:32:14 +0200

    compile warnings

--- logging.h
+++ logging.h
@@ -15,8 +15,6 @@ void DOLOG(const log_level_t ll, const char *fmt, ...);
 bool log_enabled(const log_level_t ll);
 
 #define dolog(ll, ...) do {                                     \
-        extern log_level_t log_level_file, log_level_screen;    \
-                                                                \
         if (ll >= ::log_level_file || ll >= ::log_level_screen)     \
                 DOLOG(ll __VA_OPT__(,) __VA_ARGS__);            \
         } while(0)
