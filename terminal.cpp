26203a65b4cc86bde88bf0cb023e3e160a3b3046
Folkert van Heusden <folkert@vanheusden.com>
2026-06-21 21:00:55 +0200

    show cursor

--- terminal.cpp
+++ terminal.cpp
@@ -1246,13 +1246,15 @@ void terminal::render(uint8_t **const out, int *const out_w, int *const out_h)
 	}
 
 	// cursor
-	int basic_offset = y * *out_w * char_h * 3 + x * char_w * 3;
-	for(int cy=0; cy<char_h; cy++) {
-		for(int cx=0; cx<char_w; cx++) {
-			int offset = basic_offset + cy * *out_w * 3 + cx * 3;
-			(*out)[offset + 0] ^= 255;
-			(*out)[offset + 1] ^= 255;
-			(*out)[offset + 2] ^= 255;
+	if (show_cursor) {
+		int basic_offset = y * *out_w * char_h * 3 + x * char_w * 3;
+		for(int cy=0; cy<char_h; cy++) {
+			for(int cx=0; cx<char_w; cx++) {
+				int offset = basic_offset + cy * *out_w * 3 + cx * 3;
+				(*out)[offset + 0] ^= 255;
+				(*out)[offset + 1] ^= 255;
+				(*out)[offset + 2] ^= 255;
+			}
 		}
 	}
 }
